00f2fdddf38c2392a3e27d705718200124db9218
ParadoxDjinn <paradoxdjinn@athena>
2022-03-25 21:11:11 +0100

    better c++ methods in VDEntity

--- VDEntity.h
+++ VDEntity.h
@@ -9,15 +9,15 @@
 #include "scene/main/node.h"
 
 class VDEntity : public Object {
-  GDCLASS(VDEntity, Object);
+    GDCLASS(VDEntity, Object);
 
-  static VDEntity *singleton;
+    static VDEntity *singleton;
 
-  OAHashMap<ObjectID, ObjectID> registered_nodes;
-protected:
-  static void _bind_methods();
+    OAHashMap<ObjectID, ObjectID> registered_nodes;
+  protected:
+    static void _bind_methods();
 
-public:
+  public:
     VDEntity();
 
     static _FORCE_INLINE_ VDEntity *get_singleton() { return singleton; }
@@ -31,6 +31,59 @@ public:
     bool is_entity(Node* node);
 
     List<Node*> get_entities_with(StringName aspect_name);
+
+  public:
+    template<class A>
+    _FORCE_INLINE_ static bool add(Ref<VDAspectComposer>& composer, Ref<A>& ref) {
+      return composer -> add_aspect ( Object::cast_to <VDAspect> (ref.ptr()) );
+    }
+
+    template<class A>
+    _FORCE_INLINE_ static bool remove(Ref<VDAspectComposer>& composer, Ref<A>& ref) {
+      return composer -> remove_aspect ( Object::cast_to <VDAspect> (ref.ptr()) );
+    }
+
+    template<class A>
+    _FORCE_INLINE_ static bool has(Ref<VDAspectComposer>& composer) {
+      return composer -> has_aspect ( A::get_class_static() );
+    }
+
+    template<class A>
+    _FORCE_INLINE_ static Ref<A> get(Ref<VDAspectComposer>& composer) {
+      return composer -> get_aspect ( A::get_class_static() );
+    }
+
+    template<class A>
+    _FORCE_INLINE_ static Ref<A> lookup(Ref<VDAspectComposer>& composer, Ref<A>& ref) {
+      ref = composer -> get_aspect ( A::get_class_static() );
+      return ref;
+    }
+
+  public:
+    template<class A>
+    _FORCE_INLINE_ static bool add(Node* node, Ref<A>& ref) {
+      return add <A> (singleton->get_composer(node), ref);
+    }
+
+    template<class A>
+    _FORCE_INLINE_ static bool remove(Node* node, Ref<A>& ref) {
+      return remove <A> (singleton->get_composer(node), ref);
+    }
+
+    template<class A>
+    _FORCE_INLINE_ static bool has(Node* node) {
+      return has <A> (singleton->get_composer(node));
+    }
+
+    template<class A>
+    _FORCE_INLINE_ static Ref<A> get(Node* node) {
+      return get <A> (singleton->get_composer(node));
+    }
+
+    template<class A>
+    _FORCE_INLINE_ static Ref<A> lookup(Node* node, Ref<A>& ref) {
+      return lookup <A> (singleton->get_composer(node), ref);
+    }
 
 };
 
